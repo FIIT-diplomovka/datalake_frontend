c06e5953cea5911c79d12283091fe08e5a34aced
Dominik Horvath <dom.horvath17@gmail.com>
2022-11-03 19:31:03 +0100

    initial

---
+++ components/FileUpload.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-row>
+        <v-col>
+            <h2>Please select a file</h2>
+            <v-file-input v-model="file" show-size></v-file-input>
+            <v-btn color="primary" @click="uploadFile">
+                Upload to data lake
+            </v-btn>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+    name: 'FileUpload',
+    data() {
+        return {
+            file: null,
+        }
+    },
+    methods: {
+        uploadFile(event) {
+            if (this.file === null) {
+                alert("Cannot be empty")
+            }
+            let formData = new FormData()
+            formData.append("file", this.file)
+            this.$axios.post("/upload", formData)
+            
+        }
+    }
+
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col class="text-center">
+
+      <FileUpload></FileUpload>
+      <!-- <v-btn color="primary" nuxt to="/details_page">
+      Continue
+    </v-btn> -->
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
