33d7dd40b2bf2928e7a0cd185c9d42f1cb37510c
Dominik Horvath <dom.horvath17@gmail.com>
2022-11-15 23:21:57 +0100

    browsing objects on the home page added

---
+++ components/FileBrowser.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <v-row>
+            <h2>Browse data lake</h2>
+        </v-row>
+
+        <v-row v-if="isLoading">
+            <v-col>
+                <span>Fetching objects, please wait</span>
+                <v-progress-linear indeterminate></v-progress-linear>
+            </v-col>
+        </v-row>
+
+        <v-row>
+            <v-card v-for="o in loaded_objects" class="mx-auto my-12" max-width="374">
+                <v-card-title class="text-h4">{{ o.title }}</v-card-title>
+
+                <v-card-text>
+                    <div>{{ o.description }}</div>
+                </v-card-text>
+
+                <v-divider class="mx-4"></v-divider>
+
+                <v-card-title class="text-h5">Tags</v-card-title>
+
+                <v-card-text>
+                    <v-slide-group>
+                        <v-chip v-for="tag in o.tags" class="mx-2">{{ tag }}</v-chip>
+                    </v-slide-group>
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-btn color="primary" text @click="reserve">
+                        Details
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-row>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'FileBrowser',
+    data() {
+        return {
+            loaded_objects: [],
+            isLoading: false
+        }
+    },
+    methods: {
+
+    },
+    async mounted() {
+        this.isLoading = true
+        const res = await this.$axios.get("/read/get_objects").catch(function (error) {
+            console.log(error.toJSON())
+            return
+        })
+        let res_data = res.data
+        for (const [key, value] of Object.entries(res_data)) {
+            this.loaded_objects.push({
+                title: res_data[key]["dcm_title"],
+                description: res_data[key]["dcm_description"],
+                tags: res_data[key]["tags"],
+                id: key
+            })
+        }
+        this.isLoading = false
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,19 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col class="text-center">
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col class="text-right">
         <v-btn color="primary" nuxt to="/upload">
-        Upload to datalake
-      </v-btn>
-    </v-col>
-  </v-row>
+          Upload to datalake
+        </v-btn>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <FileBrowser></FileBrowser>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
